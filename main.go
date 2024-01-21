0c6ba86e8ec313c251d5f003531ecfd20a6d93f6
Andres Ramirez <andres.ramirez9912@gmail.com>
2024-01-20 21:08:39 -0500

    Create basic project structure

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"scrapper/src/handlers"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	// Router
+	router := chi.NewRouter()
+
+	// Endpoints
+	router.Get("/scrapping", handlers.ScrappingHandler)
+
+	// Start Server
+	log.Println("Starting server at :3000 port")
+	http.ListenAndServe(":3000", router)
+
+}
